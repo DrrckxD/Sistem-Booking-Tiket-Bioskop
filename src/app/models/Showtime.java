4fb0d30cd1b052556fd51f54a20df3c74ff1c36c
Avichiss <bagusramanda81@gmail.com>
2025-12-26 03:34:08 +0700

    Update models

--- src/app/models/Showtime.java
+++ src/app/models/Showtime.java
@@ -6,14 +6,23 @@ public class Showtime implements Serializable {
     private String time;
     private int price;
     private String imagePath;
+
+    private String synopsis;
+    private String duration;
+    private String cast;
+
     public boolean[][] seats = new boolean[5][5];
 
-    public Showtime(String title, String time, int price, String imagePath) {
+    public Showtime(String title, String time, int price, String imagePath, String synopsis, String duration, String cast) {
         this.movieTitle = title;
         this.time = time;
         this.price = price;
         this.imagePath = imagePath;
+        this.synopsis = synopsis;
+        this.duration = duration;
+        this.cast = cast;
     }
+
 
     public String getTitle() { return movieTitle; }
     public void setTitle(String t) { this.movieTitle = t; }
@@ -27,5 +36,9 @@ public class Showtime implements Serializable {
     public String getImagePath() { return imagePath; }
     public void setImagePath(String p) { this.imagePath = p; }
 
-    @Override public String toString() { return movieTitle + " (" + time + ")"; }
+    public String getSynopsis() { return synopsis; }
+    public String getDuration() { return duration; }
+    public String getCast() { return cast; }
+
+    @Override public String toString() { return time; }
 }
